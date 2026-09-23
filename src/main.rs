0282a0d2c96ca1986516a81472d51b1e4e22b440
TannYuld <onuraltintas2005@gmail.com>
2026-09-23 21:10:56 +0200

    CLI parsing added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,110 @@
-use std::{env, fmt::Display, ops::{Deref, DerefMut}};
-use std::fmt::Write;
+use clap::Parser;
+use std::fmt::Write as fmtwrite;
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{Read, Write},
+    ops::{Deref, DerefMut},
+    path::PathBuf,
+};
+
+const INSTURCTIONS_MSG: &'static str = include_str!("../INSTRUCTIONS");
+
+#[derive(Parser, Debug)]
+struct CliArgs {
+    #[arg(long, value_name = "PATH")]
+    out: Option<PathBuf>,
+
+    #[arg(long("in"), value_name = "PATH")]
+    _in: Option<PathBuf>,
+
+    #[arg(long("stdin"), conflicts_with = "_in", value_name = "CODE")]
+    stdin: Option<String>,
+
+    #[arg(long, default_value_t = MachineCodeGenerationStrategy::HumanReadable)]
+    strategy: MachineCodeGenerationStrategy,
+
+    #[arg(long)]
+    instructions: bool,
+}
+
+#[derive(Debug)]
+enum LexicalInput {
+    StdIn(String),
+    File(File),
+}
+
+#[derive(Debug)]
+enum GenerationalOutput {
+    StdOut,
+    File(File),
+}
+
+#[derive(Debug)]
+struct Cli {
+    _in: LexicalInput,
+    out: GenerationalOutput,
+    strategy: MachineCodeGenerationStrategy,
+}
+
+impl TryFrom<CliArgs> for Cli {
+    type Error = CompilerError;
+
+    fn try_from(value: CliArgs) -> Result<Self, Self::Error> {
+        fn create_file(path: PathBuf) -> CompilerResult<File> {
+            File::create(path).map_err(|e| CompilerError::OsFileReadError(e))
+        }
+
+        let _in: LexicalInput = match value._in {
+            Some(path) => LexicalInput::File(create_file(path)?),
+            None => LexicalInput::StdIn(value.stdin.map_or(Err(CompilerError::NoInput), |s| Ok(s))?),
+        };
+
+        let out: GenerationalOutput = match value.out {
+            Some(path) => GenerationalOutput::File(create_file(path)?),
+            None => GenerationalOutput::StdOut,
+        };
+
+        Ok(Self {
+            _in,
+            out,
+            strategy: value.strategy,
+        })
+    }
+}
+
+impl Cli {
+    fn write_output(&mut self, text: &str) -> CompilerResult<()> {
+        match &mut self.out {
+            GenerationalOutput::StdOut => {
+                println!("{}", text);
+                Ok(())
+            }
+            GenerationalOutput::File(file) => {
+                file.write_all(text.as_bytes()).map_err(|e| CompilerError::OsFileWriteError(e))?;
+                Ok(())
+            }
+        }
+    }
+
+    fn read_input(&mut self) -> CompilerResult<String> {
+        match &mut self._in {
+            LexicalInput::StdIn(input) => Ok(input.to_string()),
+            LexicalInput::File(file) => {
+                let mut str = String::new();
+                file.read_to_string(&mut str).map_err(|e| CompilerError::OsFileReadError(e))?;
+                Ok(str)
+            }
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq)]
 struct Token<'a>(&'a str);
 
 type Tokens<'a> = Vec<Token<'a>>;
 
+#[allow(dead_code)]
 #[derive(Debug, Default)]
 enum InstructionType {
     #[default]
@@ -51,38 +150,64 @@ enum CompilerError {
     UnexpectedInstruction(String),
     NonNumericValueNotAccepted(String),
     ImmediateByteOverflow(String),
+    OsFileReadError(std::io::Error),
+    OsFileWriteError(std::io::Error),
+    NoInput,
 }
 
 type CompilerResult<T> = Result<T, CompilerError>;
 
+#[derive(clap::ValueEnum, Debug, Clone)]
 enum MachineCodeGenerationStrategy {
+    #[value(name = "pure")]
     Pure,
+
+    #[value(name = "newline")]
     NewLineSepertion,
+
+    #[value(name = "humanreadable")]
     HumanReadable,
 }
 
 trait GenerationStrategyTrait {
-    fn generate(out:&mut String, instructions: Instructions);
+    fn generate(out: &mut String, instructions: Instructions);
 }
 
 struct Pure;
 struct NewLineSeperation;
 struct HumanReadable;
 
+impl Display for MachineCodeGenerationStrategy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MachineCodeGenerationStrategy::Pure => f.write_str("pure"),
+            MachineCodeGenerationStrategy::NewLineSepertion => f.write_str("newline"),
+            MachineCodeGenerationStrategy::HumanReadable => f.write_str("humanreadable"),
+        }
+    }
+}
+
 impl GenerationStrategyTrait for Pure {
-    fn generate(out:&mut String, instructions: Instructions) {
-        todo!()
+    fn generate(out: &mut String, instructions: Instructions) {
+        *out += "0000000000000000";
+        for instruction in instructions.0.into_iter() {
+            instruction.generate(out);
+        }
     }
 }
 
 impl GenerationStrategyTrait for NewLineSeperation {
-    fn generate(out:&mut String, instructions: Instructions) {
-        todo!()
+    fn generate(out: &mut String, instructions: Instructions) {
+        *out += "0000000000000000\n";
+        for instruction in instructions.0.into_iter() {
+            instruction.generate(out);
+            *out += "\n";
+        }
     }
 }
 
 impl GenerationStrategyTrait for HumanReadable {
-    fn generate(out:&mut String, instructions: Instructions) {
+    fn generate(out: &mut String, instructions: Instructions) {
         *out += "0000 0000 00000000\n";
         for instruction in instructions.0.into_iter() {
             instruction.generate_with_space(out);
@@ -175,15 +300,41 @@ impl InstructionExpr {
     fn is_register(&self) -> bool {
         match self {
             InstructionExpr::Register(_) => true,
-            _ => false
+            _ => false,
         }
     }
 
     fn to_string(self, long_format: bool) -> String {
         match self {
             InstructionExpr::NONE => String::from("0000"),
-            InstructionExpr::Register(register) => if long_format {format!("{:08b}", register as u8)} else {format!("{:04b}", register as u8)},
-            InstructionExpr::Immediate(number, is_negative) => if long_format {format!("{:08b}", if is_negative {apply_twos_complement(number)} else {number})} else {format!("{:04b}", if is_negative {apply_twos_complement(number)} else {number})},
+            InstructionExpr::Register(register) => {
+                if long_format {
+                    format!("{:08b}", register as u8)
+                } else {
+                    format!("{:04b}", register as u8)
+                }
+            }
+            InstructionExpr::Immediate(number, is_negative) => {
+                if long_format {
+                    format!(
+                        "{:08b}",
+                        if is_negative {
+                            apply_twos_complement(number)
+                        } else {
+                            number
+                        }
+                    )
+                } else {
+                    format!(
+                        "{:04b}",
+                        if is_negative {
+                            apply_twos_complement(number)
+                        } else {
+                            number
+                        }
+                    )
+                }
+            }
         }
     }
 }
@@ -192,9 +343,17 @@ impl Instruction {
     fn _generate(self) -> (String, String, String) {
         let can_use_immediate = self._type.can_use_immediate();
         (
-            format!("{:04b}", self._type as u8 +(if self.operand.is_register() && can_use_immediate {0} else {1})),
+            format!(
+                "{:04b}",
+                self._type as u8
+                    + (if self.operand.is_register() && can_use_immediate {
+                        0
+                    } else {
+                        1
+                    })
+            ),
             self.dest.to_string(false),
-            self.operand.to_string(true)
+            self.operand.to_string(true),
         )
     }
 
@@ -235,12 +394,21 @@ impl Instructions {
 
 impl Display for CompilerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let message: String = match self {
-            CompilerError::UnexpectedInstruction(instruction) => format!("Unexpected instruction `{}`.", instruction),
-            CompilerError::NonNumericValueNotAccepted(value) => format!("Numeric value expected here `{}`.", value),
-            CompilerError::ImmediateByteOverflow(value) => format!("Number goes beyond byte limit (0..255/-128..127) `{}`.", value),
-        };
-        f.write_str(&message)
+        match self {
+            CompilerError::UnexpectedInstruction(instruction) => {
+                f.write_fmt(format_args!("Unexpected instruction `{}`.", instruction))
+            }
+            CompilerError::NonNumericValueNotAccepted(value) => {
+                f.write_fmt(format_args!("Numeric value expected here `{}`.", value))
+            }
+            CompilerError::ImmediateByteOverflow(value) => f.write_fmt(format_args!(
+                "Number goes beyond byte limit (0..255/-128..127) `{}`.",
+                value
+            )),
+            CompilerError::OsFileWriteError(e) => f.write_fmt(format_args!("Os file write error: {}", e)),
+            CompilerError::OsFileReadError(e) => f.write_fmt(format_args!("Os file read error: {}", e)),
+            CompilerError::NoInput => f.write_str("No input is given."),
+        }
     }
 }
 
@@ -319,30 +487,49 @@ fn _generate<T: GenerationStrategyTrait>(instructions: Instructions) -> String {
     result
 }
 
-fn generate(instructions: Instructions, generation_strategy: MachineCodeGenerationStrategy) -> String {
+fn generate(
+    instructions: Instructions,
+    generation_strategy: &MachineCodeGenerationStrategy,
+) -> String {
     match generation_strategy {
         MachineCodeGenerationStrategy::Pure => _generate::<Pure>(instructions),
-        MachineCodeGenerationStrategy::NewLineSepertion => _generate::<NewLineSeperation>(instructions),
-        MachineCodeGenerationStrategy::HumanReadable => _generate::<HumanReadable>(instructions)
+        MachineCodeGenerationStrategy::NewLineSepertion => {
+            _generate::<NewLineSeperation>(instructions)
+        }
+        MachineCodeGenerationStrategy::HumanReadable => _generate::<HumanReadable>(instructions),
     }
 }
 
-fn _main(text: &String) -> CompilerResult<()> {
-    let tokens = lex(&text);
+fn _assemble(mut cli: Cli) -> CompilerResult<()> {
+    let input = cli.read_input()?;
+    let tokens = lex(&input);
     let instructions = parse(tokens)?;
 
     instructions.resolve()?;
-    println!("{}", generate(instructions, MachineCodeGenerationStrategy::HumanReadable));
+    let output = generate(instructions, &cli.strategy);
+    cli.write_output(&output)?;
 
     Ok(())
 }
 
+fn _main() -> CompilerResult<bool> {
+    let cli = CliArgs::parse();
+    if cli.instructions {
+        println!("{}", INSTURCTIONS_MSG);
+        return Ok(false);
+    }
+
+    let cli: Cli = cli.try_into()?;
+    _assemble(cli)?;
+    Ok(true)
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let text = args.get(1).expect("Need ASM code");
-    match _main(text) {
-        Ok(_) => {
-            println!("Compiled!");
+    match _main() {
+        Ok(print) => {
+            if print {
+                println!("Compiled!");
+            }
         }
         Err(err) => {
             println!("{}", err);
